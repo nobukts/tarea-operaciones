17c74415e4717f914390af956fa42ffbfd0c8682
bryan404 <80658748+bryan404@users.noreply.github.com>
2023-11-02 23:13:46 -0300

    primeros pasos

---
+++ main.py
@@ -0,0 +1,91 @@
+import os
+import random
+
+def read_srflp(file_name):
+    """
+    Función para el análisis y lectura de la instancia SRFLP.
+    :param file_name: Nombre del archivo de la instancia.
+    :return: Número de instalaciones, tamaños de las instalaciones y matriz de pesos.
+    """
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+
+        n = int(lines[0].strip())
+        f_size = list(map(int, lines[1].strip().split(',')))
+        f_weight = [list(map(int, line.strip().split(','))) for line in lines[2:]]
+
+    return n, f_size, f_weight
+
+def print_instance(n, f_size, f_weight):
+    """
+    Función para imprimir la información de la instancia SRFLP.
+    :param n: Número de instalaciones.
+    :param f_size: Tamaños de las instalaciones.
+    :param f_weight: Matriz de pesos.
+    """
+    print("Numero de instalaciones:", n)
+    print("Tamaños:", " ".join(map(str, f_size)))
+    print("Pesos:")
+    for row in f_weight:
+        print(" ".join(map(str, row)))
+
+def get_weight(f_weight, i, j):
+    """
+    Función para obtener el flujo entre dos instalaciones i y j.
+    :param f_weight: Matriz de pesos.
+    :param i: Índice de la instalación i.
+    :param j: Índice de la instalación j.
+    :return: Valor del flujo entre las instalaciones i y j.
+    """
+    return f_weight[i][j]
+
+def get_facility_size(f_size, i):
+    """
+    Función para obtener el tamaño de una instalación.
+    :param f_size: Tamaños de las instalaciones.
+    :param i: Índice de la instalación i.
+    :return: Tamaño de la instalación i.
+    """
+    return f_size[i]
+
+def total_distance(n, f_size, f_weight, sol):
+    """
+    Función para calcular la distancia total recorrida por los clientes en base a la solución sol.
+    :param n: Número de instalaciones.
+    :param f_size: Tamaños de las instalaciones.
+    :param f_weight: Matriz de pesos.
+    :param sol: Arreglo con el orden de las instalaciones.
+    :return: Distancia total recorrida por los clientes.
+    """
+    total = 0.0
+    middle_distance = 0.0
+
+    for i in range(n - 1):
+        p1 = sol[i]
+        middle_distance = 0.0
+        for j in range(i + 1, n):
+            p2 = sol[j]
+            total += ( (f_size[p1] / 2 + middle_distance + f_size[p2] / 2) * (f_weight[i][j]) )
+            middle_distance += f_size[p2]
+
+    return total
+
+def solucionInicial(n):
+    arreglo = []
+    for i in range(0,n):
+        arreglo.append(i)
+    solucion = random.sample(arreglo, n)
+    return solucion
+
+#Ejemplo de uso:
+file_name = "sko100.txt"
+n, f_size, f_weight = read_srflp(file_name)
+print_instance(n, f_size, f_weight)
+#solucion = solucionInicial(n)  # Ejemplo de solución
+# 56 
+# solucion = [40, 14, 20, 24, 53, 0, 15, 3, 45, 1, 7, 36, 47, 33, 38, 25, 26, 16, 44, 5, 19, 11, 42, 30, 46, 52, 49, 27, 23, 54, 13, 6, 35, 9, 10, 28, 50, 4, 43, 55, 12, 17, 51, 8, 39, 22, 29, 41, 48, 32, 18, 21, 31, 2, 37, 34]
+# 100
+solucion = [48, 41, 38, 78, 70, 24, 31, 92, 96, 17, 4, 93, 51, 67, 7, 97, 82, 8, 15, 87, 21, 32, 42, 20, 26, 74, 79, 23, 59, 66, 85, 27, 30, 73, 18, 88, 53, 14, 0, 55, 95, 64, 3, 90, 84, 54, 12, 10, 77, 62, 56, 61, 36, 76, 58, 80, 60, 49, 91, 47, 89, 99, 37, 45, 25, 81, 68, 52, 34, 71, 65, 69, 35, 50, 9, 39, 19, 29, 46, 72, 13, 40, 86, 33, 63, 94, 43, 28, 22, 11, 16, 98, 1, 75, 57, 44, 83, 5, 2, 6]
+print("solucion: ", len(solucion))
+distancia_total = total_distance(n, f_size, f_weight, solucion)
+print("Distancia total recorrida por los clientes:", distancia_total)
