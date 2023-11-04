e89e54dedf17c9bbfe890a41a65ff1d5fe43e6e8
jjcoroneljj <jjcoroneljj@gmail.com>
2023-11-04 01:17:13 +0000

    actualización swap

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import random
 
 def read_srflp(file_name):
     """
-    Función para el análisis y lectura de la instancia SRFLP.
-    :param file_name: Nombre del archivo de la instancia.
-    :return: Número de instalaciones, tamaños de las instalaciones y matriz de pesos.
+    Función para el análisis y lectura de las instancias del problema.
+    file_name: Nombre del archivo de la instancia.
+    return: Número de instalaciones, tamaños de las instalaciones y matriz de pesos.
     """
     with open(file_name, "r") as file:
         lines = file.readlines()
@@ -19,9 +19,9 @@ def read_srflp(file_name):
 def print_instance(n, f_size, f_weight):
     """
     Función para imprimir la información de la instancia SRFLP.
-    :param n: Número de instalaciones.
-    :param f_size: Tamaños de las instalaciones.
-    :param f_weight: Matriz de pesos.
+    n: Número de instalaciones.
+    f_size: Tamaños de las instalaciones.
+    f_weight: Matriz de pesos.
     """
     print("Numero de instalaciones:", n)
     print("Tamaños:", " ".join(map(str, f_size)))
@@ -32,30 +32,30 @@ def print_instance(n, f_size, f_weight):
 def get_weight(f_weight, i, j):
     """
     Función para obtener el flujo entre dos instalaciones i y j.
-    :param f_weight: Matriz de pesos.
-    :param i: Índice de la instalación i.
-    :param j: Índice de la instalación j.
-    :return: Valor del flujo entre las instalaciones i y j.
+    f_weight: Matriz de pesos.
+    i: Índice de la instalación i.
+    j: Índice de la instalación j.
+    return: Valor del flujo entre las instalaciones i y j.
     """
     return f_weight[i][j]
 
 def get_facility_size(f_size, i):
     """
     Función para obtener el tamaño de una instalación.
-    :param f_size: Tamaños de las instalaciones.
-    :param i: Índice de la instalación i.
-    :return: Tamaño de la instalación i.
+    f_size: Tamaños de las instalaciones.
+    i: Índice de la instalación i.
+    return: Tamaño de la instalación i.
     """
     return f_size[i]
 
 def total_distance(n, f_size, f_weight, sol):
     """
     Función para calcular la distancia total recorrida por los clientes en base a la solución sol.
-    :param n: Número de instalaciones.
-    :param f_size: Tamaños de las instalaciones.
-    :param f_weight: Matriz de pesos.
-    :param sol: Arreglo con el orden de las instalaciones.
-    :return: Distancia total recorrida por los clientes.
+    n: Número de instalaciones.
+    f_size: Tamaños de las instalaciones.
+    f_weight: Matriz de pesos.
+    sol: Arreglo con el orden de las instalaciones.
+    return: Distancia total recorrida por los clientes.
     """
     total = 0.0
     middle_distance = 0.0
@@ -77,15 +77,40 @@ def solucionInicial(n):
     solucion = random.sample(arreglo, n)
     return solucion
 
+def swap(n, f_size, f_weight, sol):
+    distancia_final = total_distance(n, f_size, f_weight, sol)
+    print(f"Solucion base: ", sol)
+    print(f"Distancia total base:", distancia_final)
+    
+    sol_aux = sol
+    for i in range(0,10):
+        j1,j2 = random.sample(sol_aux, 2)
+
+        aux = sol_aux[j1]
+        sol_aux[j1] = sol_aux[j2]
+        sol_aux[j2] = aux
+
+        pos_dist = total_distance(n, f_size, f_weight, sol_aux)
+    
+        if pos_dist < distancia_final:
+            print(f"{i}    ",sol_aux)
+            print(pos_dist)
+            sol = sol_aux
+
+    return sol
+    
+
 #Ejemplo de uso:
 file_name = "sko100.txt"
 n, f_size, f_weight = read_srflp(file_name)
-print_instance(n, f_size, f_weight)
-#solucion = solucionInicial(n)  # Ejemplo de solución
+#print_instance(n, f_size, f_weight)
+dist_final = 0
+solucion = solucionInicial(n)  # Ejemplo de solución
+solucion = swap(n, f_size, f_weight, solucion)
 # 56 
-# solucion = [40, 14, 20, 24, 53, 0, 15, 3, 45, 1, 7, 36, 47, 33, 38, 25, 26, 16, 44, 5, 19, 11, 42, 30, 46, 52, 49, 27, 23, 54, 13, 6, 35, 9, 10, 28, 50, 4, 43, 55, 12, 17, 51, 8, 39, 22, 29, 41, 48, 32, 18, 21, 31, 2, 37, 34]
+#solucion = [40, 14, 20, 24, 53, 0, 15, 3, 45, 1, 7, 36, 47, 33, 38, 25, 26, 16, 44, 5, 19, 11, 42, 30, 46, 52, 49, 27, 23, 54, 13, 6, 35, 9, 10, 28, 50, 4, 43, 55, 12, 17, 51, 8, 39, 22, 29, 41, 48, 32, 18, 21, 31, 2, 37, 34]
 # 100
-solucion = [48, 41, 38, 78, 70, 24, 31, 92, 96, 17, 4, 93, 51, 67, 7, 97, 82, 8, 15, 87, 21, 32, 42, 20, 26, 74, 79, 23, 59, 66, 85, 27, 30, 73, 18, 88, 53, 14, 0, 55, 95, 64, 3, 90, 84, 54, 12, 10, 77, 62, 56, 61, 36, 76, 58, 80, 60, 49, 91, 47, 89, 99, 37, 45, 25, 81, 68, 52, 34, 71, 65, 69, 35, 50, 9, 39, 19, 29, 46, 72, 13, 40, 86, 33, 63, 94, 43, 28, 22, 11, 16, 98, 1, 75, 57, 44, 83, 5, 2, 6]
-print("solucion: ", len(solucion))
-distancia_total = total_distance(n, f_size, f_weight, solucion)
-print("Distancia total recorrida por los clientes:", distancia_total)
+#solucion = [48, 41, 38, 78, 70, 24, 31, 92, 96, 17, 4, 93, 51, 67, 7, 97, 82, 8, 15, 87, 21, 32, 42, 20, 26, 74, 79, 23, 59, 66, 85, 27, 30, 73, 18, 88, 53, 14, 0, 55, 95, 64, 3, 90, 84, 54, 12, 10, 77, 62, 56, 61, 36, 76, 58, 80, 60, 49, 91, 47, 89, 99, 37, 45, 25, 81, 68, 52, 34, 71, 65, 69, 35, 50, 9, 39, 19, 29, 46, 72, 13, 40, 86, 33, 63, 94, 43, 28, 22, 11, 16, 98, 1, 75, 57, 44, 83, 5, 2, 6]
+
+print(f"\nSolucion final: ", solucion)
+print(f"Distancia total final:", total_distance(n, f_size, f_weight, solucion))
