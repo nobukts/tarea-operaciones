c3d58397dcb7a089a5effb159a62d9b11a45ec83
jjcoroneljj <jjcoroneljj@gmail.com>
2023-11-05 18:35:43 +0000

    Actualización

--- main.py
+++ main.py
@@ -80,9 +80,9 @@ def solucionInicial(n):
 def swap(n, f_size, f_weight, sol):
     dist_ori = total_distance(n, f_size, f_weight, sol)
     mod_arr = sol.copy()
-    print(f"Solución Base: {sol}\nDistancia base:{dist_ori}\n") 
+    print(f"Solución Base: {sol}\nDistancia base: {dist_ori}\n") 
 
-    t_i = 20
+    t_i = round(dist_ori/2)
     t_m = 0.2
     alpha = 0.4
 
@@ -99,29 +99,30 @@ def swap(n, f_size, f_weight, sol):
         if dif_dist < 0:
             sol = mod_arr.copy()
             dist_ori = pos_dist
-            print(f"Solución Criterio 1: {sol}\nDistancia nueva:{dist_ori}") 
+            print(f"Solución Criterio 1: {dist_ori}") 
         elif m.exp(-(dif_dist)/(t_i)) > r.random():
             sol = mod_arr.copy()
             dist_ori = pos_dist
-            print(f"Solución Criterio 2: {sol}\nDistancia nueva:{dist_ori}") 
+            print(f"Solución Criterio 2: {dist_ori}") 
         t_i *= alpha
         
     return sol
     
 
 #Elección de archivo
-file_name = "S8.txt"
+file_name = "sko56.txt"
 n, f_size, f_weight = read_srflp(file_name)
 
 print_instance(n, f_size, f_weight)
 print("\n===========================\n")
 solucion = solucionInicial(n)  # Solución generada de forma aleatoria uniforme
-solucion = swap(n, f_size, f_weight, solucion) #Método swap para encontrar una mejor solución
 
 # Óptimo para 56 tiendas 
 #solucion = [40, 14, 20, 24, 53, 0, 15, 3, 45, 1, 7, 36, 47, 33, 38, 25, 26, 16, 44, 5, 19, 11, 42, 30, 46, 52, 49, 27, 23, 54, 13, 6, 35, 9, 10, 28, 50, 4, 43, 55, 12, 17, 51, 8, 39, 22, 29, 41, 48, 32, 18, 21, 31, 2, 37, 34]
 # Óptimo para 100 tiendas 
 #solucion = [48, 41, 38, 78, 70, 24, 31, 92, 96, 17, 4, 93, 51, 67, 7, 97, 82, 8, 15, 87, 21, 32, 42, 20, 26, 74, 79, 23, 59, 66, 85, 27, 30, 73, 18, 88, 53, 14, 0, 55, 95, 64, 3, 90, 84, 54, 12, 10, 77, 62, 56, 61, 36, 76, 58, 80, 60, 49, 91, 47, 89, 99, 37, 45, 25, 81, 68, 52, 34, 71, 65, 69, 35, 50, 9, 39, 19, 29, 46, 72, 13, 40, 86, 33, 63, 94, 43, 28, 22, 11, 16, 98, 1, 75, 57, 44, 83, 5, 2, 6]
+
+solucion = swap(n, f_size, f_weight, solucion) #Método swap para encontrar una mejor solución
 
 print(f"\nSolucion final: ", solucion)
 print(f"Distancia total final:", total_distance(n, f_size, f_weight, solucion))
